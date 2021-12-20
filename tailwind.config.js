5cbe83c0830513181a87f064925d8b964bccffa7
Amritpal Singh <matharooamrit098@gmail.com>
2021-12-20 14:21:53 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      height:{
+        '68':'17rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
